a184e75bad7513d83caf0bc24bba58e8d9fdd0c3
Federico Rossi <federico.s.rossi@gmail.com>
2023-06-05 21:18:28 -0300

    feat: :sparkles: adding translations to app

--- vue-multilingual/src/components/HelloWorld.vue
+++ vue-multilingual/src/components/HelloWorld.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ $t('welcomeMessage') }}</h1>
+    <img class="language-flag" src="../images/us.png" @click="setLanguage('en')">
+    <img class="language-flag" src="../images/es.png" @click="setLanguage('es')">
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      {{ $t('guide') }}
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>{{ $t('installedCliPlugins') }}</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>{{ $t('essentialLinks') }}</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">{{ $t('coreDocs') }}</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">{{ $t('forum') }}</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">{{ $t('communityChat') }}</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">{{ $t('twitter') }}</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">{{ $t('news') }}</a></li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>{{ $t('ecosystem') }}</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">{{ $t('vueRouter') }}</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">{{ $t('vuex') }}</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">{{ $t('vueDevtools') }}</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">{{ $t('vueLoader') }}</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">{{ $t('awesomeVue') }}</a></li>
     </ul>
   </div>
 </template>
@@ -32,13 +33,14 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  methods: {
+    setLanguage(language) {
+      this.$i18n.locale = language;
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -53,5 +55,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.language-flag {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  width: 40px;
+  height: 20px;
+  margin-right: 10px;
 }
 </style>
--- vue-multilingual/src/main.js
+++ vue-multilingual/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import App from "./App.vue";
+import { messages } from "./core/traslations";
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  locale: "en",
+  messages,
+});
+createApp(App).use(i18n).mount("#app");
